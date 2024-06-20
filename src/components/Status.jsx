90f1e92a0c8ac6a7eb3ea8fbc660a944c4edea62
Brinda Muraleedharan <brinda.salesforce@gmail.com>
2024-06-20 13:20:59 -0400

    Change in title

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,14 +1,10 @@
-import styles from './status.module.css'
-let Status=({completedTasks,totalTasks})=>{
-    return(<div className={styles.footer}>
-        <span className={styles.item}>
-        Completed To Dos:{completedTasks}
-        </span>
-        <span className={styles.item}>
-        Total To Dos:{totalTasks}
-        </span>
-        
-    </div>);
-
-}
+import styles from "./status.module.css";
+let Status = ({ completedTasks, totalTasks }) => {
+  return (
+    <div className={styles.footer}>
+      <span className={styles.item}>Completed To Dos:{completedTasks}</span>
+      <span className={styles.item}>Total To Dos:{totalTasks}</span>
+    </div>
+  );
+};
 export default Status;
